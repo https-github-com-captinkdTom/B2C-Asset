69dc78e37f8114ed1ae23673c1b71eb5c20a977a
jenna <jenna@greenit.cc>
2022-08-29 15:22:35 +0900

    no message

--- src/Components/Asset.js
+++ src/Components/Asset.js
@@ -6,7 +6,7 @@ function Asset(props) {
             <h2>{props.name}</h2>
             <h3>{props.type}</h3>
             <h3>{props.id}</h3>
-            {/*<p>{props.person.email}</p>*/}
+            <p>{props.person.email}</p>
         </li>
     );
 }
